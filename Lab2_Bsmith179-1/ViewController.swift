9caeab4f6f50ff48c254dba5363292caccf91550
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-02-16 14:14:06 -0500

    Added Header for ViewController.swift

--- Lab2_Bsmith179-1/ViewController.swift
+++ Lab2_Bsmith179-1/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
 //  Lab2_Bsmith179-1
-//
-//  Created by Icy on 2/16/26.
+//  App shows a card style sample restaurent menu item.
+//  Created by Brigitte on 2/16/26.
 //
 
 import UIKit
